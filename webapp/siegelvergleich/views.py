c1115bb951362a803c43c96af3523840c42310f2
lfrer <luise.frerichs@gmx.de>
2022-01-24 12:24:12 +0100

    hähnchen nud schwein uch mit tabelle

--- webapp/siegelvergleich/views.py
+++ webapp/siegelvergleich/views.py
@@ -41,8 +41,34 @@ def Rind(request):
 #  return render(request, 'siegelvergleich/rind.html', context={'Objekte': rind_Siegel, 'erste_wahl': siegel_1, 'zweite_wahl': siegel_2}) 
 
 def Haehnchen(request):    
-    return render(request, 'siegelvergleich/hähnchen.html')
+    siegel_1=""
+    siegel_2=""
+    rind_Siegel=Siegel.objects.filter(tier='Hähnchen')
+    if request.method == "POST":  
+        id1=request.POST['siegel1']
+        id2=request.POST['siegel2']
+        siegel_1=Siegel.objects.filter(id=id1)  
+        siegel_2=Siegel.objects.filter(id=id2)  
+
+        return render(request, 'siegelvergleich/hähnchenwahl.html', context={'Objekte': rind_Siegel,'erste_wahl':siegel_1,'zweite_wahl':siegel_2})     
+
+    else:
+        return render(request, 'siegelvergleich/hähnchen.html', context={'Objekte': rind_Siegel})
+  #  return render(request, 'siegelvergleich/hähnchen.html')
 
 
 def Schwein(request):    
-    return render(request, 'siegelvergleich/schwein.html')
+    siegel_1=""
+    siegel_2=""
+    rind_Siegel=Siegel.objects.filter(tier='Schwein')
+    if request.method == "POST":  
+        id1=request.POST['siegel1']
+        id2=request.POST['siegel2']
+        siegel_1=Siegel.objects.filter(id=id1)  
+        siegel_2=Siegel.objects.filter(id=id2)  
+
+        return render(request, 'siegelvergleich/schweinwahl.html', context={'Objekte': rind_Siegel,'erste_wahl':siegel_1,'zweite_wahl':siegel_2})     
+
+    else:
+        return render(request, 'siegelvergleich/schwein.html', context={'Objekte': rind_Siegel})
+   #  return render(request, 'siegelvergleich/schwein.html')
