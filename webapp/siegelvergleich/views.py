946bc4be96bcb54b68f204ca94a259be789f3080
lfrer <luise.frerichs@gmx.de>
2022-01-24 16:34:28 +0100

    Fehlermeldung bei leerer eingabe

--- webapp/siegelvergleich/views.py
+++ webapp/siegelvergleich/views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+#from asyncio.windows_events import NULL
+#from contextlib import nullcontext
 
 from django.shortcuts import render
 from django.http import HttpResponse, request
@@ -15,12 +17,17 @@ def Rind(request):
     siegel_2=""
     rind_Siegel=Siegel.objects.filter(tier='Rind')
     if request.method == "POST":  
-        id1=request.POST['siegel1']
-        id2=request.POST['siegel2']
-        siegel_1=Siegel.objects.filter(id=id1)  
-        siegel_2=Siegel.objects.filter(id=id2)  
+        if (request.POST['siegel1'] != "null" and request.POST['siegel2'] != "null"):
+            id1=request.POST['siegel1']
+            id2=request.POST['siegel2']
+            siegel_1=Siegel.objects.filter(id=id1)  
+            siegel_2=Siegel.objects.filter(id=id2)  
+            warnung=''
+            return render(request, 'siegelvergleich/rindwahl.html', context={'Objekte': rind_Siegel,'erste_wahl':siegel_1,'zweite_wahl':siegel_2, 'warnung':warnung})     
 
-        return render(request, 'siegelvergleich/rindwahl.html', context={'Objekte': rind_Siegel,'erste_wahl':siegel_1,'zweite_wahl':siegel_2})     
+        else:
+            warnung='Bitte wähle zwei Siegel zum Vergleich aus.'
+            return render(request, 'siegelvergleich/rind.html', context={'Objekte': rind_Siegel,'warnung':warnung})     
 
     else:
         return render(request, 'siegelvergleich/rind.html', context={'Objekte': rind_Siegel})
@@ -43,17 +50,23 @@ def Rind(request):
 def Haehnchen(request):    
     siegel_1=""
     siegel_2=""
-    hähnchen_Siegel=Siegel.objects.filter(tier='Hähnchen')
+    warnung={}
+    haehnchen_Siegel=Siegel.objects.filter(tier='Hähnchen')
     if request.method == "POST":  
-        id1=request.POST['siegel1']
-        id2=request.POST['siegel2']
-        siegel_1=Siegel.objects.filter(id=id1)  
-        siegel_2=Siegel.objects.filter(id=id2)  
+        if (request.POST['siegel1'] != "null" and request.POST['siegel2'] != "null"):
+            id1=request.POST['siegel1']
+            id2=request.POST['siegel2']
+            siegel_1=Siegel.objects.filter(id=id1)  
+            siegel_2=Siegel.objects.filter(id=id2)  
+            warnung=''
+            return render(request, 'siegelvergleich/hähnchenwahl.html', context={'Objekte': haehnchen_Siegel,'erste_wahl':siegel_1,'zweite_wahl':siegel_2, 'warnung':warnung})     
 
-        return render(request, 'siegelvergleich/hähnchenwahl.html', context={'Objekte': hähnchen_Siegel,'erste_wahl':siegel_1,'zweite_wahl':siegel_2})     
+        else:
+            warnung='Bitte wähle zwei Siegel zum Vergleich aus.'
+            return render(request, 'siegelvergleich/hähnchen.html', context={'Objekte': haehnchen_Siegel,'warnung':warnung})     
 
     else:
-        return render(request, 'siegelvergleich/hähnchen.html', context={'Objekte': hähnchen_Siegel})
+        return render(request, 'siegelvergleich/hähnchen.html', context={'Objekte': haehnchen_Siegel})
   #  return render(request, 'siegelvergleich/hähnchen.html')
 
 
@@ -62,12 +75,17 @@ def Schwein(request):
     siegel_2=""
     schwein_Siegel=Siegel.objects.filter(tier='Schwein')
     if request.method == "POST":  
-        id1=request.POST['siegel1']
-        id2=request.POST['siegel2']
-        siegel_1=Siegel.objects.filter(id=id1)  
-        siegel_2=Siegel.objects.filter(id=id2)  
+        if (request.POST['siegel1'] != "null" and request.POST['siegel2'] != "null"):
+            id1=request.POST['siegel1']
+            id2=request.POST['siegel2']
+            siegel_1=Siegel.objects.filter(id=id1)  
+            siegel_2=Siegel.objects.filter(id=id2)  
+            warnung=''
+            return render(request, 'siegelvergleich/schweinwahl.html', context={'Objekte': schwein_Siegel,'erste_wahl':siegel_1,'zweite_wahl':siegel_2, 'warnung':warnung})     
 
-        return render(request, 'siegelvergleich/schweinwahl.html', context={'Objekte': schwein_Siegel,'erste_wahl':siegel_1,'zweite_wahl':siegel_2})     
+        else:
+            warnung='Bitte wähle zwei Siegel zum Vergleich aus.'
+            return render(request, 'siegelvergleich/schwein.html', context={'Objekte': schwein_Siegel,'warnung':warnung})     
 
     else:
         return render(request, 'siegelvergleich/schwein.html', context={'Objekte': schwein_Siegel})
