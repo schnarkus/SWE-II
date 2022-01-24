5fd26349e451ba38150de0d093c49507095cead2
ultimatempert <tempestglodowski@gmail.com>
2022-01-24 11:38:10 +0100

    Datenbank vervollständigt

---
+++ webapp/db2.sql
@@ -0,0 +1,461 @@
+/* Alle Tiere Tierschutz kontrolliert "Gold" von VIER PFOTEN */
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Tierschutz kontrolliert „Gold“ von VIER PFOTEN', 'Hähnchen',
+'Im Stall max. 21 kg pro m²,
+max. 4.800 Tiere pro Stall,
+zusätzlich zum Stall ist ein
+Außenklimabereich sowie
+eine Freilandfläche mit mind.
+4 m² / Tier vorgeschrieben.', 'Konventionelles Futter.
+Gentechnisch veränderte
+Futtermittel verboten.', 'Schnäbelkürzen verboten.
+Prophylaktische Gabe von
+Antibiotika nicht zulässig.', 'Max. 4 Stunden Transportzeit.');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Tierschutz kontrolliert „Gold“ von VIER PFOTEN', 'Rind',
+'< 200 kg – 2,5 m² / Tier im
+Stall + 2 m² im Auslauf.
+< 350 kg – 4 m² / Tier im
+Stall + 3 m² im Auslauf.
+Für jede weitere 100 kg über
+350 kg plus 1m im Stall und
+mehr Platz im Auslauf.
+Mind. 150 Tage Weidegang
+von mind. 6 Stunden im Jahr.
+Vollspaltenböden verboten.
+Einstreu im Liegebereich
+verpflichtend. Jederzeit
+Zugang zu befestigtem
+Auslauf.', 'Konventionelles Futter.
+Gentechnisch veränderte
+Futtermittel verboten.', 'Verhindern des Hornwachs-
+tums ist verboten, Kastration
+nur mit Betäubung erlaubt.
+Prophylaktische Gabe von
+Antibiotika nicht zulässig', 'Max. 4 Stunden Transportzeit.');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Tierschutz kontrolliert „Gold“ von VIER PFOTEN', 'Schwein',
+'< 50 kg – 0,8 m² / Tier im
+Stall + 0,6 m² im Auslauf
+< 85 kg – 1,1 m² / Tier im
+Stall + 0,8 m² im Auslauf
+< 110 kg – 1,3 m² / Tier im
+Stall + 1,0 m² im Auslauf
+> 110 kg – 1,5 m² / Tier im
+Stall + 1,2 m² im Auslauf
+Liegebereich ohne Spalten
+mit Einstreu.
+Jederzeit zugänglicher,
+befestigter Auslauf
+verpflichtend.', 'Konventionelles Futter.
+Gentechnisch veränderte
+Futtermittel verboten.', 'Ferkelkastration nur mit
+Betäubung und
+Schmerzmitteln erlaubt.
+Abschneiden von
+Ringelschwänzen und
+routinemäßiges Zähnekürzen
+verboten.
+Prophylaktische Gabe von
+Antibiotika nicht zulässig.', 'Max. 4 Stunden Transportzeit.');
+
+/* Alle Tiere Tierschutz kontrolliert "Silber" von VIER PFOTEN */
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Tierschutz kontrolliert „Silber“ von VIER PFOTEN', 'Hähnchen',
+'m Stall max. 25 kg pro m²,
+max. 30.000 Tiere pro Stall,
+zusätzlich zum Stall ist ein
+Außenklimabereich
+vorgeschrieben, aber keine
+Freilandfläche.', 'Konventionelles Futter.
+Gentechnisch veränderte
+Futtermittel verboten.', 'Schnäbelkürzen verboten.
+Prophylaktische Gabe von
+Antibiotika nicht zulässig.', 'Max. 4 Stunden Transportzeit.');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Tierschutz kontrolliert „Silber“ von VIER PFOTEN', 'Rind',
+'< 200 kg – 2,5 m² / Tier
+< 350 kg – 4 m² / Tier
+Für jede weitere 100 kg über
+350 kg plus 1 m².
+Kein Auslauf, aber mind. 120
+Tage Weidegang von mind.
+6 Stunden im Jahr.
+Vollspaltenböden verboten.', 'Konventionelles Futter.
+Gentechnisch veränderte
+Futtermittel verboten.', 'Enthornung und Kastration
+nur mit Betäubung und
+Schmerzmitteln erlaubt.
+Prophylaktische Gabe von
+Antibiotika nicht zulässig.', 'Max. 4 Stunden Transportzeit.');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Tierschutz kontrolliert „Silber“ von VIER PFOTEN', 'Schwein',
+'< 85 kg – 1,1 m² / Tier
+< 110 kg – 1,4 m² / Tier
+> 110 kg – 2,0 m² / Tier
+Max. 1.500 Mastplätze pro
+Durchgang. Auslauf oder
+Außenklimabereich
+vorgeschrieben. Liegebereich
+ohne Spalten mit Einstreu.', 'Konventionelles Futter.
+Gentechnisch veränderte
+Futtermittel sind verboten.', 'Ferkelkastration nur mit
+Betäubung und
+Schmerzmitteln erlaubt.
+Abschneiden von
+Ringelschwänzen und
+routinemäßiges Zähnekürzen
+sind verboten.
+Prophylaktische Gabe von
+Antibiotika nicht zulässig.', 'Max. 4 Stunden Transportzeit.');
+
+/* Hähnchen und Schwein DTB Premium */
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('DTB Premium', 'Hähnchen', 'm Stall max. 21 kg bzw.
+10 Tiere pro m², max. 16.000
+Tiere pro Stall, zusätzlich zum
+Stall ist ein befestigter,
+überdachter Außenklima-
+bereich sowie eine
+Freilandfläche mit mind. 4 m²
+/ Tier vorgeschrieben.', 'Konventionelles Futter.
+Verbot von gentechnisch
+veränderten Futtermitteln.', 'Schnäbelkürzen prinzipiell
+erlaubt.
+Prophylaktische Gabe von
+Antibiotika nicht zulässig.', 'Max. 4 Stunden Transportzeit.');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('DTB Premium', 'Schwein',
+'< 50 kg – 0,5 m² / Tier im
+Stall + 0,3 m² im Auslauf
+< 120 kg – 1,0 m² / Tier im
+Stall + 0,5 m² im Auslauf
+> 120 kg – 1,5 m² / Tier im
+Stall + 0,8 m² im Auslauf
+Max. 3.000 Tiere /
+Eigentümer. Einstreu auf
+Liegefläche vorgeschrieben.
+Keine Spaltenböden im
+Liegebereich. Getrennter
+Liege- und Aktivitätsbereich.
+Stroh zur Beschäftigung.
+Auslauf ins Freie ist Pflicht.',
+'Konventionelles Futter.
+Gentechnisch veränderte
+Futtermittel verboten.', 
+'Ferkelkastration nur mit
+Betäubung und Schmerz-
+mittel; Schwänzekupieren
+verboten.
+Prophylaktische Gabe von
+Antibiotika nicht zulässig, nur
+Behandlung einzelner Tiere
+erlaubt.', 'Max. 4 Stunden Transportzeit.');
+
+/* Hähnchen und Schwein DTB Einstieg */
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('DTB Einstieg', 'Hähnchen',
+'Im Stall max. 25 kg bzw.
+15 Tiere pro m², max. 30.000
+Tiere pro Stall, zusätzlich zum
+Stall ist ein befestigter,
+überdachter Außenklima-
+bereich vorgeschrieben.
+Kein Freilandbereich für die
+Hühner', 'Konventionelles Futter.
+Verbot von gentechnisch
+veränderten Futtermitteln', 'Schnäbelkürzen prinzipiell
+erlaubt.
+Prophylaktische Gabe von
+Antibiotika nicht zulässig.', 'Max. 4 Stunden Transportzeit');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('DTB Einstieg', 'Schwein',
+'< 40 kg – 0,55 m² / Tier
+im Stall
+< 120 kg – 1,1 m² / Tier
+im Stall
+> 120 kg – 1,6 m² / Tier
+im Stall
+Max. 3.000 Tiere /
+Eigentümer. Spaltenböden.
+Kein Einstreu vorgeschrieben.
+Getrennter Liege- und
+Aktivitätsbereich. Stroh zur
+Beschäftigung. Auslauf ins
+Freie keine Pflicht.', 'Konventionelles Futter.
+Gentechnisch veränderte
+Futtermittel verboten.', 'Ferkelkastration nur mit
+Betäubung und Schmerz-
+mittel; Schwänzekupieren
+verboten.
+Prophylaktische Gabe von
+Antibiotika nicht zulässig, nur
+Behandlung einzelner Tiere
+erlaubt.', 'Max. 4 Stunden Transportzeit.');
+
+/* Alle Tiere QS */
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('QS', 'Hähnchen',
+'Im Stall max. 39 kg pro m²,
+keine Stallobergrenze, kein
+Außenklimabereich, keine
+Freifläche.', 'Konventionelles Futter.', 'Schnäbelkürzen prinzipiell
+erlaubt.
+Prophylaktische Gabe von
+Antibiotika nicht zulässig.', 'Nach Gesetz – ohne verkürzte
+Transportdauer (in der EU bei
+Inlandstransporten max.
+8 Stunden, aber mit vielen
+Ausnahmen).');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('QS', 'Rind',
+'< 150 kg – 1,5 m² / Tier
+< 220 kg – 1,7 m² / Tier
+< 400 kg – 1,8 m² / Tier
+> 400 kg – 2,2 m² / Tier
+Im Stall auf Spaltenböden.
+Kein Auslauf oder Weidegang
+vorgeschrieben.', 'Konventionelles Futter.
+Gentechnisch veränderte
+Futtermittel erlaubt.', 'Enthornung bei Kälbern unter
+6 Wochen ohne Betäubung
+erlaubt.
+Prophylaktische Gabe von
+Antibiotika nicht zulässig.', 'Nach Gesetz – ohne verkürzte
+Transportdauer (in der EU bei
+Inlandstransporten max.
+8 Stunden, aber mit vielen
+Ausnahmen).');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('QS', 'Schwein', '< 10 kg – 0,15 m² / Tier
+< 20 kg – 0,20 m² / Tier
+< 30 kg – 0,35 m² / Tier
+< 50 kg – 0,50 m² / Tier
+< 110 kg – 0,75 m² / Tier
+> 110 kg – 1,00 m² / Tier
+Auf Spaltenböden im Stall.
+Keine Bestandsobergrenze.
+Kein Auslauf vorgeschrieben.', 'Konventionelles Futter.
+Gentechnik erlaubt.', 'Ferkelkastration ohne
+Betäubung erlaubt;
+Schwänzekupieren und
+Zähnekürzen sind nicht
+verboten.
+Prophylaktische Gabe von
+Antibiotika nicht zulässig.', 'Nach Gesetz – ohne verkürzte
+Transportdauer (in der EU bei
+Inlandstransporten max. 8
+Stunden, aber mit vielen
+Ausnahmen).');
+
+/* Für Hähnchen und Schwein Initiative Tierwohl*/
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Initiative Tierwohl', 'Hähnchen',
+'Im Stall max. 35 kg pro m²,
+keine Stallobergrenze, kein
+Außenklimabereich, keine
+Freifläche.', 'Konventionelles Futter.', 'Schnäbelkürzen prinzipiell
+erlaubt.
+Keine Angaben zu
+Medikamentenverboten.', 'Keine Angaben zur
+Transportdauer.');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Initiative Tierwohl', 'Schwein',
+'Haltungskriterien
+unterscheiden sich gering
+vom gesetzlichen
+Mindeststandard.
+< 30 kg – 0,385 m² / Tier
+< 50 kg – 0,55 m² / Tier
+< 110 kg – 0,825 m² / Tier
+> 110 kg – 1,10 m² / Tier
+Keine Angabe zur
+Bestandsobergrenze.
+Zusätzliches organisches
+Beschäftigungsmaterial.', 'Konventionelles Futter.
+Gentechnik erlaubt.', 'Keine Angaben zu Kastration,
+Abschneiden von
+Ringelschwänzen oder zur
+prophylaktischen Gabe von
+Antibiotika.', 'Keine Angaben zur
+Transportdauer.');
+
+/* Alle Tiere Haltungsform 1 (Stallhaltung) */
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Haltungsform 1 (Stallhaltung)', 'Hähnchen',
+'Im Stall max. 39 kg pro m²,
+keine Stallobergrenze, kein
+Außenklimabereich, keine
+Freifläche.', 'Konventionelles Futter.', 'Schnäbelkürzen prinzipiell
+erlaubt.
+Keine Angaben zu
+Medikamentenverboten.', 'Keine Angaben zur
+Transportdauer.');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Haltungsform 1 (Stallhaltung)', 'Rind',
+'< 150 kg – 1,5 m² / Tier
+< 220 kg – 1,7 m² / Tier
+< 400 kg – 1,8 m² / Tier
+> 400 kg – 2,2 m² / Tier
+Kein Auslauf oder Weidegang
+vorgeschrieben.', 'Konventionelles Futter.
+Gentechnik erlaubt.', 'Enthornung bei Kälbern unter
+6 Wochen ohne Betäubung
+erlaubt.
+Keine Angaben zur
+prophylaktischen Gabe von
+Antibiotika.', 'Keine Angaben zur
+Transportdauer.');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Haltungsform 1 (Stallhaltung)', 'Schwein',
+'< 110 kg – 0,75 m² / Tier
+Kein Auslauf vorgeschrieben.', 'Konventionelles Futter.
+Gentechnik erlaubt.', 'Keine Angaben zu Kastration,
+Abschneiden von
+Ringelschwänzen oder zur
+prophylaktischen Gabe von
+Antibiotika.', 'Keine Angaben zur
+Transportdauer.');
+
+/* Alle Tiere Haltungsform 2 (Stallhaltung plus) */
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Haltungsform 2 (Stallhaltung plus)', 'Hähnchen',
+'Im Stall max. 35 kg pro m²,
+keine Stallobergrenze, kein
+Außenklimabereich, keine
+Freifläche.', 'Konventionelles Futter.', 'Schnäbelkürzen prinzipiell
+erlaubt.
+Keine Angaben zu
+Medikamentenverboten.', 'Keine Angaben zur
+Transportdauer.');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Haltungsform 2 (Stallhaltung plus)', 'Rind',
+'< 150 kg – 1,5 m² / Tier
+< 220 kg – 1,8 m² / Tier
+< 400 kg – 2,5 m² / Tier
+> 400 kg – 3,0 m² / Tier
+Kein Auslauf oder Weidegang
+vorgeschrieben.', 'Konventionelles Futter.
+Gentechnik erlaubt.', 'Enthornung bei Kälbern unter
+6 Wochen ohne Betäubung
+erlaubt.
+Keine Angaben zur
+prophylaktischen Gabe von
+Antibiotika.', 'Keine Angaben zur
+Transportdauer.');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Haltungsform 2 (Stallhaltung plus)', 'Schwein',
+'< 110 kg – 0,825 m² / Tier
+Kein Auslauf vorgeschrieben.
+Zusätzliches organisches
+Beschäftigungsmaterial.', 'Konventionelles Futter.
+Gentechnik erlaubt.', 'Keine Angaben zu Kastration,
+Abschneiden von
+Ringelschwänzen oder zur
+prophylaktischen Gabe von
+Antibiotika.', 'Keine Angaben zur
+Transportdauer.');
+
+/* Alle Tiere Haltungsform 3 (Außenklima) */
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Haltungsform 3 (Außenklima)', 'Hähnchen',
+'Im Stall max. 25 kg pro m²,
+keine Stallobergrenze,
+zusätzlich zum Stall ist ein
+befestigter, überdachter
+Außenklimabereich
+vorgeschrieben, keine
+Freifläche.', 'Konventionelles Futter.
+Gentechnisch veränderte
+Futtermittel sind verboten.', 'Schnäbelkürzen prinzipiell
+erlaubt.
+Keine Angaben zu
+Medikamentenverboten.', 'Keine Angaben zur
+Transportdauer.');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Haltungsform 3 (Außenklima)', 'Rind',
+'Haltung und Auslauf
+< 150 kg – 1,5 m² / Tier
+< 220 kg – 2 m² / Tier
+< 400 kg – 3 m² / Tier
+> 400 kg – 4 m² / Tier
+Entweder ständiger Auslauf
+oder Weidegang an mind.
+120 Tagen von mind.
+6 Stunden.', 'Konventionelles Futter.
+Gentechnik erlaubt.', 'Enthornung bei Kälbern unter
+6 Wochen ohne Betäubung
+erlaubt.
+Keine Angaben zur
+prophylaktischen Gabe von
+Antibiotika.', 'Keine Angaben zur
+Transportdauer.');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Haltungsform 3 (Außenklima)', 'Schwein',
+'<110 kg – 1,05 m²/Tier
+Kein Auslauf vorgeschrieben,
+aber Stallhaltung mit
+Außenklimareizen.
+Zusätzliches organisches
+Beschäftigungsmaterial.', 'Konventionelles Futter.
+Gentechnik erlaubt.', 'Keine Angaben zu Kastration,
+Abschneiden von
+Ringelschwänzen oder zur
+prophylaktischen Gabe von
+Antibiotika.', 'Keine Angaben zur
+Transportdauer.');
+
+/* Alle Tiere Haltungsform 4 (Premium) */
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Haltungsform 4 (Premium)', 'Hähnchen',
+'Im Stall max. 21 kg pro m²,
+keine Stallobergrenze,
+zusätzlich zum Stall ist ein
+befestigter, überdachter
+Außenklimabereich sowie
+eine Freilandfläche
+vorgeschrieben.', 'Konventionelles Futter; mind.
+20 % Futter aus dem eigenen
+Betrieb bzw. aus der Region.
+Gentechnisch veränderte
+Futtermittel sind verboten.', 'Schnäbelkürzen prinzipiell
+erlaubt.
+Keine Angaben zu
+Medikamentenverboten.', 'Keine Angaben zur
+Transportdauer.');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Haltungsform 4 (Premium)', 'Rind',
+'< 150 kg – 1,5 m² / Tier
+< 220 kg – 2,5 m² / Tier
+< 400 kg – 4 m² / Tier
+> 400 kg – 5 m² / Tier bzw.
+mind. 1m² / 100 kg
+Ständiger Auslauf und
+Weidegang an mind. 120
+Tagen von mind. 6 Stunden.', 'Konventionelles Futter; mind.
+60 % Futtermittel aus dem
+eigenen Betrieb bzw. aus der
+Region
+Gentechnisch veränderte
+Futtermittel verboten.', 'Enthornung nur nach
+Betäubung erlaubt.
+Keine Angaben zur
+prophylaktischen Gabe von
+Antibiotika.', 'Keine Angaben zur
+Transportdauer.');
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('Haltungsform 4 (Premium)', 'Schwein',
+'< 110 kg – 1,5 m² / Tier
+Auslauf vorgeschrieben.
+Zusätzliches organisches
+Beschäftigungsmaterial.', 'Konventionelles Futter; mind.
+20 % aus eigenem Betrieb
+oder aus der Region.
+Gentechnik verboten.', 'Keine Angaben zu Kastration,
+Abschneiden von
+Ringelschwänzen oder zur
+prophylaktischen Gabe von
+Antibiotika.', 'Keine Angaben zur
+Transportdauer.');
+
+
