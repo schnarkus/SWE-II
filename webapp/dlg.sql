3112f8b7b4454af3d3e66edf58798722da28acfa
ultimatempert <tempestglodowski@gmail.com>
2022-02-03 22:13:51 +0100

    DLG Siegel zur Datenbank hinzugefügt

---
+++ webapp/dlg.sql
@@ -0,0 +1,13 @@
+/* Alle Tiere DLG-Prämierung Gold/Silber/Bronze */
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('DLG-Prämierung Gold/Silber/Bronze', 'Hähnchen', 'Die Siegel geben keine Auskunft darüber, wie die Tiere gehalten wurden.', 'Die Siegel geben keine Auskunft darüber, ob Gentechnik und Antibiotika zum Einsatz kamen.', 'Die Siegel geben keine Auskunft darüber, ob Gentechnik und Antibiotika zum Einsatz kamen.',
+'Die Siegel geben keine Auskunft darüber, wie die Tiere gehalten wurden.'
+);
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('DLG-Prämierung Gold/Silber/Bronze', 'Rind', 'Die Siegel geben keine Auskunft darüber, wie die Tiere gehalten wurden.', 'Die Siegel geben keine Auskunft darüber, ob Gentechnik und Antibiotika zum Einsatz kamen.', 'Die Siegel geben keine Auskunft darüber, ob Gentechnik und Antibiotika zum Einsatz kamen.',
+'Die Siegel geben keine Auskunft darüber, wie die Tiere gehalten wurden.'
+);
+
+INSERT INTO Siegel (Bezeichnung, Tier, Haltung_Auslauf, Futter, Gesundheit, Transport) VALUES ('DLG-Prämierung Gold/Silber/Bronze', 'Schwein', 'Die Siegel geben keine Auskunft darüber, wie die Tiere gehalten wurden.', 'Die Siegel geben keine Auskunft darüber, ob Gentechnik und Antibiotika zum Einsatz kamen.', 'Die Siegel geben keine Auskunft darüber, ob Gentechnik und Antibiotika zum Einsatz kamen.',
+'Die Siegel geben keine Auskunft darüber, wie die Tiere gehalten wurden.'
+);
