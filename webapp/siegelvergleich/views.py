e353d62140fef4db20dfdad6c3cfec674fb9ada3
lfrer <luise.frerichs@gmx.de>
2022-02-03 12:44:58 +0100

    neues design auch bei tabelle

--- webapp/siegelvergleich/views.py
+++ webapp/siegelvergleich/views.py
@@ -23,7 +23,7 @@ def Rind(request):
             siegel_1=Siegel.objects.filter(id=id1)  
             siegel_2=Siegel.objects.filter(id=id2)  
             warnung=''
-            return render(request, 'siegelvergleich/rindwahl.html', context={'Objekte': rind_Siegel,'erste_wahl':siegel_1,'zweite_wahl':siegel_2, 'warnung':warnung})     
+            return render(request, 'siegelvergleich/rindwahl_neuesdesign.html', context={'Objekte': rind_Siegel,'erste_wahl':siegel_1,'zweite_wahl':siegel_2, 'warnung':warnung})     
 
         else:
             warnung='Bitte wähle zwei Siegel zum Vergleich aus.'
@@ -45,7 +45,7 @@ def Haehnchen(request):
             siegel_1=Siegel.objects.filter(id=id1)  
             siegel_2=Siegel.objects.filter(id=id2)  
             warnung=''
-            return render(request, 'siegelvergleich/hähnchenwahl.html', context={'Objekte': haehnchen_Siegel,'erste_wahl':siegel_1,'zweite_wahl':siegel_2, 'warnung':warnung})     
+            return render(request, 'siegelvergleich/hähnchenwahl_neuesdesign.html', context={'Objekte': haehnchen_Siegel,'erste_wahl':siegel_1,'zweite_wahl':siegel_2, 'warnung':warnung})     
 
         else:
             warnung='Bitte wähle zwei Siegel zum Vergleich aus.'
@@ -66,7 +66,7 @@ def Schwein(request):
             siegel_1=Siegel.objects.filter(id=id1)  
             siegel_2=Siegel.objects.filter(id=id2)  
             warnung=''
-            return render(request, 'siegelvergleich/schweinwahl.html', context={'Objekte': schwein_Siegel,'erste_wahl':siegel_1,'zweite_wahl':siegel_2, 'warnung':warnung})     
+            return render(request, 'siegelvergleich/schweinwahl_neuesdesign.html', context={'Objekte': schwein_Siegel,'erste_wahl':siegel_1,'zweite_wahl':siegel_2, 'warnung':warnung})     
 
         else:
             warnung='Bitte wähle zwei Siegel zum Vergleich aus.'
