e40c1eae13db75dcdcc055415c63bd9cdd68227e
schnarkus <89841238+schnarkus@users.noreply.github.com>
2022-02-10 12:11:29 +0100

    Sequenzdiagramm hinzugefügt

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const burgerIcon = document.querySelector('#burger');
-const navbarMenu = document.querySelector('#nav-links');
+const burgerIcon = document.querySelector("#burger");
+const navbarMenu = document.querySelector("#nav-links");
 
-burgerIcon.addEventListener('click', () => {
-  navbarMenu.classList.toggle('is-active');
+burgerIcon.addEventListener("click", () => {
+  navbarMenu.classList.toggle("is-active");
 });
 
-const tabs = document.querySelectorAll('.tabs li');
-const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+const tabs = document.querySelectorAll(".tabs li");
+const tabContentBoxes = document.querySelectorAll("#tab-content > div");
 
-tabs.forEach(tab => {
-  tab.addEventListener('click', () => {
-    tabs.forEach(item => item.classList.remove('is-active'));
-    tab.classList.add('is-active');
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((item) => item.classList.remove("is-active"));
+    tab.classList.add("is-active");
 
     const target = tab.dataset.target;
 
-    tabContentBoxes.forEach(box => {
-      if (box.getAttribute('id') === target) {
-        box.classList.remove('is-hidden');
+    tabContentBoxes.forEach((box) => {
+      if (box.getAttribute("id") === target) {
+        box.classList.remove("is-hidden");
       } else {
-        box.classList.add('is-hidden');
+        box.classList.add("is-hidden");
       }
-    })
-  })
-})
+    });
+  });
+});
